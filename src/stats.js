36efa8ef058ecabc661803e3e55f8acc3f3ed5a4
Michal <michal83h@gmail.com>
2017-09-29 13:33:03 +0200

    last fixes

--- src/stats.js
+++ src/stats.js
@@ -1,8 +1,63 @@
 const output = require('./output');
+const nodegit = require('nodegit');
+
+const stats = {
+  committers: {},
+  startDate: null,
+  hours: {},
+  hoursCount: {},
+};
+
+const render = () => {
+  let txt = `Project started on ${stats.startDate.toISOString()}\n`;
+  txt += 'Contributor\t Commits\t Hours\n';
+  const data = [];
+  Object.keys(stats.committers).forEach((committer) => {
+    data.push({
+      name: committer,
+      commits: stats.committers[committer],
+      hours: stats.hoursCount[committer],
+    });
+  });
+  data.sort((a, b) => (a.commits > b.commits ? -1 : 1));
+  for (let i = 0; i < data.length; i += 1) {
+    txt += `${data[i].name}\t ${data[i].commits}\t ${data[i].hours}\n`;
+  }
+  output.output(txt);
+};
+
+const getCommitterName = (committer) => {
+  let name = committer.email();
+  if (!name) name = committer.name();
+  return name;
+};
+
+const commit2stats = (commit) => {
+  const commiter = getCommitterName(commit.committer());
+  const date = commit.date();
+  const day = `${date.getDate()}-${date.getMonth()}-${date.getFullYear()}`;
+  const hours = date.getHours();
+  if (!stats.committers[commiter]) stats.committers[commiter] = 1;
+  else stats.committers[commiter] += 1;
+  if (!stats.startDate) stats.startDate = date;
+  else if (stats.startDate.getTime() > date.getTime()) stats.startDate = date;
+  if (!stats.hours[commiter]) stats.hours[commiter] = {};
+  if (!stats.hoursCount[commiter]) stats.hoursCount[commiter] = 0;
+  if (!stats.hours[commiter][day]) stats.hours[commiter][day] = {};
+  if (!stats.hours[commiter][day][hours]) {
+    stats.hours[commiter][day][hours] = true;
+    stats.hoursCount[commiter] += 1;
+  }
+  commit.free();
+};
 
 module.exports = (repo) => {
   repo.getMasterCommit()
-  .then((commit) => {
-    console.log('commit', commit);
+  .then((lastCommit) => {
+    const history = lastCommit.history(nodegit.Revwalk.SORT.TIME);
+    history.on('commit', commit2stats);
+    history.on('error', output.error);
+    history.on('end', render);
+    history.start();
   }, output.error);
 };
