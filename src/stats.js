626fefd3e11a1f9adbcdabc66b47239fba05a8b1
Michal <michal83h@gmail.com>
2017-10-02 13:43:10 +0200

    last fixes

--- src/stats.js
+++ src/stats.js
@@ -4,12 +4,16 @@ const nodegit = require('nodegit');
 const stats = {
   committers: {},
   startDate: null,
+  totalHours: 0,
+  totalCommits: 0,
   hours: {},
   hoursCount: {},
 };
 
 const render = () => {
-  let txt = `Project started on ${stats.startDate.toISOString()}\n`;
+  let txt = `Project started:\t ${stats.startDate.toISOString()}\n`;
+  txt += `Commits:\t ${stats.totalCommits}\n`;
+  txt += `Hours:\t ${stats.totalHours}\n\n`;
   txt += 'Contributor\t Commits\t Hours\n';
   const data = [];
   Object.keys(stats.committers).forEach((committer) => {
@@ -47,17 +51,22 @@ const commit2stats = (commit) => {
   if (!stats.hours[commiter][day][hours]) {
     stats.hours[commiter][day][hours] = true;
     stats.hoursCount[commiter] += 1;
+    stats.totalHours += 1;
   }
+  stats.totalCommits += 1;
   commit.free();
 };
 
 module.exports = (repo) => {
-  repo.getMasterCommit()
-  .then((lastCommit) => {
-    const history = lastCommit.history(nodegit.Revwalk.SORT.TIME);
-    history.on('commit', commit2stats);
-    history.on('error', output.error);
-    history.on('end', render);
-    history.start();
+  repo.fetchAll()
+  .then(() => {
+    repo.getHeadCommit()
+    .then((headCommit) => {
+      const history = headCommit.history(nodegit.Revwalk.SORT.TIME);
+      history.on('commit', commit2stats);
+      history.on('error', output.error);
+      history.on('end', render);
+      history.start();
+    }, output.error);
   }, output.error);
 };
